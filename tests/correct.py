3299adf142ad2a4f4b7e8e209b230ce3ee3ab6a2
Esha Gupta <66334834+eshagupta01@users.noreply.github.com>
2023-11-15 07:51:01 -0800

    Zybooks 7.7 function

--- tests/correct.py
+++ tests/correct.py
@@ -32,9 +32,33 @@ def get_dictionary_value(options, option):
             return_str += key + ', '
         return return_str[:-2]
 
+def modify_list(num_list):
+    """
+    modify_list() takes a list (num_list).
+    The function modifies the list of numbers by adding their position value to the corresponding value in the list.
+    For example: If the list is [1, 2, 3, 4, 5] the function should return the modified list with the values [1, 3, 5, 7, 9]
+    If num_list contains a value that isn't an integer, then return the statement 'List contains type that is not an integer at position {pos}'
+    where pos is first instance of the other type
+    If num_list is empty, return -1
+    Note: Please refer to Zybooks 7.7 for more information on modifying lists.
+    """
+    if len(num_list) == 0:
+        return -1
+    for i in range(len(num_list)):
+        if type(num_list[i]) is int:
+            num_list[i] += i
+        else:
+            return f'List contains type that is not an integer at position {i}'
+    return num_list
+
+
 if __name__ == "__main__":
     ### Write 3 assert statements
     ### to test the function
     assert get_dictionary_value({}, 'a') == -1
     assert get_dictionary_value({'a': 1, 'b': 2, 'c': 3}, 'b') == 2
     assert get_dictionary_value({'a': 1, 'b': 2, 'c': 3}, 'd') == 'The available options are - a, b, c'
+
+    assert modify_list([1, 2, 3, 4, 5]) == [1, 3, 5, 7, 9]
+    assert modify_list(['hello', 10]) == 'List contains type that is not an integer at position 0'
+    assert modify_list([]) == -1
